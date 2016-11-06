c0069a40fb8132d05183dbec830789a0358f7f1d
Kristoffer Eklund <kristoffereklund1@gmail.com>
2016-11-06 21:04:36 +0100

    alpha_space translation & custom ExceptionHandler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Auth\AuthenticationException;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Bitaac\Core\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
