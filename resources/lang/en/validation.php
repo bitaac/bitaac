7b73ec556316902513478105920df52ae9a9ebdd
eklundkristoffer <eklukris@gmail.com>
2016-11-06 21:04:36 +0100

    alpha_space translation & custom ExceptionHandler

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -83,6 +83,7 @@ return [
     'unique'               => 'The :attribute has already been taken.',
     'uploaded'             => 'The :attribute failed to upload.',
     'url'                  => 'The :attribute format is invalid.',
+    "alpha_space"          => "The :attribute may only contain letters and spaces.",
 
     /*
     |--------------------------------------------------------------------------
